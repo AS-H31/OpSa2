52492a6e45f1052d9edec5e8fadeaf3f0ad757a5
Abobakr Al-mahdi <74077606+Abobakral-mahdi@users.noreply.github.com>
2021-10-20 13:38:53 +0200

    Com

--- Basisanwendung/src/gui/Main.java
+++ Basisanwendung/src/gui/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Guten Tag!");
-		System.out.println("Ahmed This is Bakr");
-		//Hallo
+		
+		//Hallo ahmed
 	}
 
 }
