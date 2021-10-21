665ab1ee092dc745c45f1e6950dcbb29c20ce541
Ahmad Hesso <shiar.hesso@gmail.com>
2021-10-21 15:59:09 +0200

    Changes made to Anwendersystem and Main

--- Basisanwendung/src/gui/Main.java
+++ Basisanwendung/src/gui/Main.java
@@ -1,12 +1,18 @@
 package gui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		System.out.println("Guten Tag!");
-		System.out.println("Guten Tag Ahmed");
-		//Hallo Ahmed This is Abobakr
-		
-	}
 
+public class Main extends Application {
+	
+    	@Override
+    	public void start(Stage primaryStage) {
+        	new Anwendersystem(primaryStage);
+    	}	
+    
+    	public static void main(String[] args){ 
+	  	launch(args);
+    	}
 }
+
